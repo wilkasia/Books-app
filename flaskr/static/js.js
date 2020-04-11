20a5240dec8250b7780f7d1dcba3c45e678b6231
Wojciech Wilanski <wwilanski@gmail.com>
2020-04-11 23:05:31 +0200

    new gui

---
+++ flaskr/static/js.js
@@ -0,0 +1,21 @@
+require([
+    'dojo/_base/url',
+    'dojo/dom',
+    'dojo/dom-class',
+    'dojo/dom-attr',
+    'dojo/query',
+    'dojo/ready'
+], function (url,dom,domClass,domAttr,query,ready) {
+    ready(function () {
+        const loc = window.location.pathname;
+        console.log('loc ',window.location.pathname);
+
+        query('.vertical-menu > a').forEach(function (node) {
+            if(domAttr.get(node,'href')===loc) {
+                domClass.add(node,'active');
+            } else {
+                domClass.remove(node,'active');
+            }
+        });
+    });
+});
